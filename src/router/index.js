85dcb9dd070fb38799aed887a2d5cc1ece4bbdfa
unknown <1766758704@qq.com>
2022-01-13 22:01:06 +0800

    init Gshop

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+// 路由器对象模块
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../pages/Home/Home.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Search from '../pages/Search/Search.vue'
+import Login from '../pages/Login/Login.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/home',
+      component: Home,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/',
+      redirect: '/home',
+      meta: {
+        showFooter: true
+      }
+    }
+  ]
+})
